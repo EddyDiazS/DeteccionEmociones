04070134784e0c9e80b244a041509fb8333fa429
EddyDiaz <eddya.diazs@konradlorenz.edu.co>
2024-05-30 06:32:50 -0500

    Final

--- script.js
+++ script.js
@@ -13,29 +13,22 @@ const MODEL_URL = 'models';
         if (file) {
             const imageURL = URL.createObjectURL(file);
 
-            // Esperar a que la imagen se cargue completamente
             const image = new Image();
             image.onload = async () => {
-                // Ajustar el tamaño del canvas a la imagen
+                
                 canvas.width = image.width;
                 canvas.height = image.height;
 
-                // Dibujar la imagen en el canvas
                 ctx.drawImage(image, 0, 0);
 
-                // Detectar todas las caras y sus expresiones en la imagen
                 let fullFaceDescriptions = await faceapi.detectAllFaces(canvas).withFaceExpressions();
 
-                // Limpiar el canvas antes de dibujar
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-                // Dibujar la imagen y luego las detecciones y las expresiones faciales en el canvas
                 ctx.drawImage(image, 0, 0);
                 faceapi.draw.drawDetections(canvas, fullFaceDescriptions);
-                faceapi.draw.drawFaceExpressions(canvas, fullFaceDescriptions, 0.5); // Puedes ajustar el umbral aquí si lo deseas
+                faceapi.draw.drawFaceExpressions(canvas, fullFaceDescriptions, 0.5);
             };
 
-            // Asignar la URL de la imagen para que se inicie la carga
             image.src = imageURL;
         }
     });
